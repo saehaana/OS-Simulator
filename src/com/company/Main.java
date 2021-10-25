0b0b3c848b22c592cc3c394311a386f108332b59
saehaana <61268356+saehaana@users.noreply.github.com>
2021-10-24 23:06:10 -0400

    allow user to choose two different programs to create processes for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //template files
         BufferedReader brCalculator = new BufferedReader(new FileReader("C:\\Users\\ccc\\Downloads\\School\\6 Fall 2021\\CMSC 312\\cmsc312\\Calculator.txt"));
         BufferedReader brGame = new BufferedReader(new FileReader("C:\\Users\\ccc\\Downloads\\School\\6 Fall 2021\\CMSC 312\\cmsc312\\Game.txt"));
         BufferedReader brMicrosoftWord = new BufferedReader(new FileReader("C:\\Users\\ccc\\Downloads\\School\\6 Fall 2021\\CMSC 312\\cmsc312\\MicrosoftWord.txt"));
@@ -14,86 +15,70 @@ public class Main {
         String currLine;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Select up to 2 programs you would like to run from list below:");
+        System.out.println("Select up to 2 different programs you would like to run from list below:");
         System.out.println("Calculator," + " Game," + " Microsoft Word," + " Storage");
         System.out.println("Programs: ");
-        String processName = scanner.nextLine();
+        for(int i=0;i<2;i++){
+            String processName = scanner.nextLine();
 
-        if(processName.equalsIgnoreCase("Calculator")){
-            while((currLine = brCalculator.readLine()) != null) {
-                if(currLine.equals("CALCULATE")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(5,100));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+            //different operations with cycles given based on program user selects
+            if(processName.equalsIgnoreCase("Calculator")){
+                while((currLine = brCalculator.readLine()) != null) {
+                    if(currLine.equals("CALCULATE")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(5,100));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
+                    if(currLine.equals("I/O")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(11,82));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
                 }
-                if(currLine.equals("I/O")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(11,82));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+            }
+            if(processName.equalsIgnoreCase("Game")){
+                while((currLine = brGame.readLine()) != null) {
+                    if(currLine.equals("CALCULATE")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(5,100));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
+                    if(currLine.equals("I/O")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(11,82));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
+                }
+            }
+            if(processName.equalsIgnoreCase("Microsoft Word")){
+                while((currLine = brMicrosoftWord.readLine()) != null) {
+                    if(currLine.equals("CALCULATE")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(5,100));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
+                    if(currLine.equals("I/O")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(11,82));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
+                }
+            }
+            if(processName.equalsIgnoreCase("Storage")){
+                while((currLine = brStorage.readLine()) != null) {
+                    if(currLine.equals("CALCULATE")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(5,100));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
+                    if(currLine.equals("I/O")){
+                        process.setState("NEW");
+                        process.setCycle(getRandomNumber(11,82));
+                        System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                    }
                 }
             }
         }
-        if(processName.equalsIgnoreCase("Game")){
-            while((currLine = brGame.readLine()) != null) {
-                if(currLine.equals("CALCULATE")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(5,100));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-                }
-                if(currLine.equals("I/O")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(11,82));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-                }
-            }
-        }
-        if(processName.equalsIgnoreCase("Microsoft Word")){
-            while((currLine = brMicrosoftWord.readLine()) != null) {
-                if(currLine.equals("CALCULATE")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(5,100));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-                }
-                if(currLine.equals("I/O")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(11,82));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-                }
-            }
-        }
-        if(processName.equalsIgnoreCase("Storage")){
-            while((currLine = brStorage.readLine()) != null) {
-                if(currLine.equals("CALCULATE")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(5,100));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-                }
-                if(currLine.equals("I/O")){
-                    process.setState("NEW");
-                    process.setCycle(getRandomNumber(11,82));
-                    System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-                }
-            }
-        }
-
-
-
-
-
-        /**while((currLine = brCalculator.readLine()) != null) {
-            if(currLine.equals("CALCULATE")){
-                process.setState("NEW");
-                process.setCycle(getRandomNumber(5,100));
-                System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-            }
-            if(currLine.equals("I/O")){
-                process.setState("NEW");
-                process.setCycle(getRandomNumber(11,82));
-                System.out.println(process.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
-            }
-        }**/
-
-
     }
     /**Generates random number within a certain range; to be used as a representation of cycles for processes
      * min: lower bound of random number range
