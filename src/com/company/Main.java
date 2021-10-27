c64355a9e9afd0b314420fda4db493ceb80906e9
saehaana <61268356+saehaana@users.noreply.github.com>
2021-10-26 23:21:18 -0400

    moved pcb getState() to case 2 of switch statement, added more print statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,22 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //objects and variables
+        //class objects
         Scanner scanner = new Scanner(System.in);
         Process process = new Process();
         PCB pcb = new PCB();
         CriticalSection cs = new CriticalSection();
         Scheduler scheduler = new Scheduler();
+
+        //PCB class objects
+        PCB pcbCalculator = new PCB();
+        PCB pcbGame = new PCB();
+        PCB pcbMicrosoftWord = new PCB();
+        PCB pcbStorage = new PCB();
+
+        //variables
         String currLine;
         int menuSelection;
-        boolean loop = true;
 
         //template files
         BufferedReader brCalculator = new BufferedReader(new FileReader("C:\\Users\\ccc\\Downloads\\School\\6 Fall 2021\\CMSC 312\\cmsc312\\Calculator.txt"));
@@ -25,7 +32,7 @@ public class Main {
         System.out.println("Welcome to Ausawin's OS simulator!");
         System.out.println("****************************************");
         System.out.println("Choose any of the options below by pressing the corresponding number");
-        System.out.println("Command Options: ");
+        System.out.println("Once you've entered an option, type 'return' to go back to main menu");
         System.out.println("1: Open applications");
         System.out.println("2: Check process states");
         System.out.println("3: Check PCB components");
@@ -40,72 +47,66 @@ public class Main {
                 case 1:
                     System.out.println("You chose option 1");
                     System.out.println("Select up to 2 different programs you would like to run from list below:");
-                    System.out.println("Type 'return' to return back to main menu");
                     System.out.println("Calculator," + " Game," + " Microsoft Word," + " Storage");
                     System.out.println("Programs: ");
                     while(scanner.hasNextLine()){
                             String processName = scanner.nextLine();
                             //different operations with cycles given based on program user selects
                             if(processName.equalsIgnoreCase("Calculator")){
+                                pcbCalculator.setState("NEW");
                                 while((currLine = brCalculator.readLine()) != null) {
                                     if(currLine.equals("CALCULATE")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(5,100));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                     if(currLine.equals("I/O")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(11,82));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                 }
                             }
                             if(processName.equalsIgnoreCase("Game")){
+                                pcbGame.setState("NEW");
                                 while((currLine = brGame.readLine()) != null) {
                                     if(currLine.equals("CALCULATE")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(5,100));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                     if(currLine.equals("I/O")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(11,82));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                 }
                             }
                             if(processName.equalsIgnoreCase("Microsoft Word")){
+                                pcbMicrosoftWord .setState("NEW");
                                 while((currLine = brMicrosoftWord.readLine()) != null) {
                                     if(currLine.equals("CALCULATE")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(5,100));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                     if(currLine.equals("I/O")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(11,82));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                 }
                             }
                             if(processName.equalsIgnoreCase("Storage")){
+                                pcbStorage.setState("NEW");
                                 while((currLine = brStorage.readLine()) != null) {
                                     if(currLine.equals("CALCULATE")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(5,100));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                     if(currLine.equals("I/O")){
-                                        pcb.setState("NEW");
                                         process.setCycle(getRandomNumber(11,82));
-                                        System.out.println(pcb.getState() + " " + currLine + " given " + process.getCycle() + " cycles");
+                                        System.out.println(currLine + " given " + process.getCycle() + " cycles");
                                     }
                                 }
                             }
                             if(processName.equalsIgnoreCase("return")){
                                 System.out.println("Returning to main menu...\n");
                                 System.out.println("Choose any of the options below by pressing the corresponding number");
-                                System.out.println("Command Options: ");
                                 System.out.println("1: Open applications");
                                 System.out.println("2: Check process states");
                                 System.out.println("3: Check PCB components");
@@ -113,13 +114,47 @@ public class Main {
                                 System.out.println("0: Quit");
                                 break;
                             }
+                            //if((!(processName.equalsIgnoreCase("calculator")) || (!(processName.equalsIgnoreCase("game")))) || (!(processName.equalsIgnoreCase("microsoft word"))) || (!(processName.equalsIgnoreCase("storage"))) || (!(processName.equalsIgnoreCase("return")))) {
+                            //    System.out.println("Unknown option, try again");
+                            //}
                         }
                     break;
                 case 2:
                     System.out.println("You chose option 2");
+                    System.out.println("Which application's process state would you like to check?");
+                    while(scanner.hasNextLine()) {
+                        String processName = scanner.nextLine();
+
+                        if(processName.equalsIgnoreCase("Calculator")){
+                            System.out.println("Process state of " + processName + " is: " + pcbCalculator.getState());
+                        }
+                        else if(processName.equalsIgnoreCase("Game")){
+                            System.out.println("Process state of " + processName + " is: " + pcbGame.getState());
+                        }
+                        else if(processName.equalsIgnoreCase("Microsoft Word")){
+                            System.out.println("Process state of " + processName + " is: " + pcbMicrosoftWord.getState());
+                        }
+                        else if(processName.equalsIgnoreCase("Storage")){
+                            System.out.println("Process state of " + processName + " is: " + pcbStorage.getState());
+                        }
+                        else if(processName.equalsIgnoreCase("return")) {
+                            System.out.println("Returning to main menu...\n");
+                            System.out.println("Choose any of the options below by pressing the corresponding number");
+                            System.out.println("1: Open applications");
+                            System.out.println("2: Check process states");
+                            System.out.println("3: Check PCB components");
+                            System.out.println("4: Check something else");
+                            System.out.println("0: Quit");
+                            break;
+                        }
+                        else{
+                            System.out.println("Please enter one of the listed applications you would like to check their processes' state for...or check your spelling");
+                        }
+                    }
                     break;
                 case 3:
                     System.out.println("You chose option 3");
+                    System.out.println("Which PCB component would you like to check?");
                     break;
                 case 4:
                     System.out.println("You chose option 4");
